720bab6f6b0797e665b9ac92e8e9235c96c36693
mrbodja <mrbodja@gmail.com>
2019-05-20 02:58:58 +0300

    created class

--- PurseGit/PurseGit/BusinessCard.cs
+++ PurseGit/PurseGit/BusinessCard.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace PurseGit
 {
-    class Program
+    class BusinessCard
     {
-        static void Main(string[] args)
-        {
-            BusinessCard businessCard = new BusinessCard();
-        }
     }
 }
